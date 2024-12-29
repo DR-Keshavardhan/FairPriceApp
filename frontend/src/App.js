c95a02b8b6b779230450bd75acd2fb2db918d477
kavirajmetech <kavirajmetech@gmail.com>
2024-12-29 09:13:43 +0530

    login true

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import ButtonPage from './buttonPage'; // Import the ButtonPage component
 import Login from './components/login1'; // Import your Login1 component
 import Login2 from './components/login2'; // Import your Login2 component
-import UploadExcel from './components/UploadExcel'; // Import UploadExcel component
+import UploadExcel from './components/UploadExcel1.js'; // Import UploadExcel component
 import MainApp from './App'; // Import the MainApp or app logic component (ensure to rename if necessary)
 import TalukPage from './components/TalukPage'; // Import TalukPage
 import DistrictPage from './components/DistrictPage'; // Import DistrictPage
 import StatePage from './components/StatePage'; // Import StatePage
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false); // Track authentication state
+  const [isAuthenticated, setIsAuthenticated] = useState(true); // Track authentication state
 
   return (
     <Router>
